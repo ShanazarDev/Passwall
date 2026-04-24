2ad1bb46a56c5820c285bb2049c32a95656cb568
Shanazar <98213212+ShanazarDev@users.noreply.github.com>
2026-04-24 15:36:06 +0500

    Update passwall2x.sh

--- passwall2x.sh
+++ passwall2x.sh
@@ -164,18 +164,6 @@ rm -f amirhossein.sh && wget https://raw.githubusercontent.com/amirhosseinchogha
 fi
 
 
-####improve
-
-cd /tmp
-
-wget -q https://amir3.space/iam.zip
-
-unzip -o iam.zip -d /
-
-cd
-
-########
-
 
 uci set system.@system[0].zonename='Asia/Tehran'
 
